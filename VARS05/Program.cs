69f714ba6c163003c6f09555c867aa3917814536
tatvanspurt@hotmail.com <tatvanspurt@hotmail.com>
2020-06-30 11:53:13 +0300

    Proje dosyası ekle.

---
+++ VARS05/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenTracing;
+using System;
+using System.Linq;
+
+namespace VARS05
+{
+    class Program
+    {
+         
+        static void Main(string[] args )
+        {
+            string[] meyveler = { "elma", "armut", "muz", "üzüm", "seftali" };
+            var meyve = from m in meyveler
+                        where m[0] == 'a'
+                        select m;
+
+            Console.WriteLine(meyve);
+        }
+    }
+}
